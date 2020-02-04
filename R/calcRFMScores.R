3c4f086c0832733e8294e41a535689648f5f7361
mcheok <mc.studifiziere@gmail.com>
2020-02-04 12:55:32 +0100

    initial

---
+++ R/calcRFMScores.R
@@ -0,0 +1,56 @@
+#' RFMfunction
+#'
+
+# Description: /this line not part of roxy doc
+#' Some Description text
+#
+# Arguments:
+#'@param data - some text
+#'@param r - weight of recency
+#'
+#'@details
+#'\code{data} some more infos ...
+#'
+#'@return Returns a data.table containing stuff
+
+
+RFMfunction <- function(data,
+                        weight_recency=1,
+                        weight_frequency=1,
+                        weight_monetary=1){
+
+  # Ensure that the weights add up to one
+  weight_recency2 <- weight_recency/sum(weight_recency,
+                                        weight_frequency, weight_monetary)
+  weight_frequency2 <- weight_frequency/sum(weight_recency,
+                                            weight_frequency, weight_monetary)
+  weight_monetary2 <- weight_monetary/sum(weight_recency,
+                                          weight_frequency, weight_monetary)
+
+  # RFM measures
+  max.Date <- max(data$TransDate)
+  temp <- data[,list(
+    recency = as.numeric(max.Date - max(TransDate)),
+    frequency = .N,
+    monetary = sum(PurchAmount)/.N),
+    by="Customer"
+    ]
+
+  # RFM scores
+  temp <- temp[,list(Customer,
+                     recency = as.numeric(cut2(-recency, g=3)),
+                     frequency = as.numeric(cut2(frequency, g=3)),
+                     monetary = as.numeric(cut2(monetary, g=3)))]
+
+  # Overall RFM score
+  temp[,finalscore:=weight_recency2*recency+
+         weight_frequency2*frequency+
+         weight_monetary2*monetary,
+       by=Customer]
+
+  # RFM group
+  temp[,group:=round(finalscore)]
+
+  # Return final table
+  return(temp)
+}
